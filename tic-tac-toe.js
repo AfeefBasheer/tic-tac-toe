b43c9aa807764008c278779586459bcb3a369c00
Afeef Basheer <afeefmb.4221@gmail.com>
2022-09-18 17:06:37 +0530

    print matrix commit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,8 +4,29 @@ function createMatrix() {
   for (let i = 0; i < 3; i++) {
     mat[i] = [];
     for (let j = 0; j < 3; j++) {
-     mat[i]="-";
+      mat[i][j] = "-";
     }
   }
 }
-console.log(createMatrix()); 
+
+createMatrix();
+
+function printMatrix(mat) {
+  let string = " ┌─────┬─────┬─────┐" + "\n";
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      string = string + " |  " + mat[i][j] + " ";
+    }
+    string = string + " |" + "\n";
+    if (i != 2) {
+      string = string + " ├─────┼─────┼─────┤";
+    }
+
+    string = string + "\n";
+  }
+  string=string.trimEnd();
+  string = string +"\n"+ " └─────┴─────┴─────┘";
+  return string;
+}
+console.log(printMatrix(mat));
